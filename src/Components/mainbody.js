536614ae6476b24da692dfc091833d875f1ce747
ss10ss10 <sulaiman.sultan96@gmail.com>
2022-08-08 18:01:59 +0500

    Stats added

--- src/Components/mainbody.js
+++ src/Components/mainbody.js
@@ -5,9 +5,15 @@ import '../Styles/mainbody.css';
 
 const Mainbody = () => {
     return (
-        <div className='container'>
-            <img src={hamilton} className="card1"></img>
-            <img src={alex} className="card2"></img>
+        <div className='card'>
+            <img src={hamilton} className="card1-image"></img>
+            <div className="card1-stats">
+                <span>5.0</span>
+                <span className="gray">(6) • USA</span>
+            </div>
+            <p className='card1-stats'>Life Lessons with Hamilton</p>
+            <p className='card1-stats'><span className="bold">From $136</span> / person</p>
+            <img src={alex} className="card2-image"></img>
         </div>
     );
 }
